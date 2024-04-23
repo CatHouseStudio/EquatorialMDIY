0a2920920cca7362a717b58c181f99e4c3fb5d71
lyf <luyifeng3@163.com>
2024-04-23 16:28:02 +0800

    add new web server support and OTA support change board from mega2560 to esp32

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,14 @@
-// Include Arduino FreeRTOS library
 
 #include "task.hpp"
-#include <Arduino.h>
-#include "StepperMotor.hpp"
-#include "EquatorialMount.hpp"
 
-//替换引脚编号
-StepperMotor motor(STEP_PIN, DIR_PIN);
 
 void setup()
 {
   Semaphore_Init();
   task_Create();
-  Serial.begin(115200);
-  // motor.setGearRatio(2.0);
-  setupMotorControl();
-  setMotorSpeed(94);
-  
 }
 
 void loop() {
-//  motor.rotateWithEarth();
 
-  stepMotor(1, HIGH);
-  //考虑减速比之后设置的步数
-  delay(3600000 / 94);
+
 }
