461cfecfeabf4699810b474c18398033ea123156
lyf <luyifeng3@163.com>
2024-04-24 10:51:58 +0800

    UPDATE delete useless code add stepper motor work(PWM method)

--- src/SerialMessage.hpp
+++ src/SerialMessage.hpp
@@ -9,7 +9,7 @@ static QueueHandle_t queueHandle_Serial0;
 void Serial0_Message_Queue_Init(void)
 {
     queueHandle_Serial0 = xQueueCreate(100, sizeof(char *));
-    Serial.begin(115200);
+    Serial.begin(Serial_0_Bit_Rate);
     if (queueHandle_Serial0 == NULL)
     {
         Serial.println("Queue init error!!");
